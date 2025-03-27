78ce4eda10a5f841321a1adfb0b3e70e2389d972
KevinC76 <xckevinxc67@gmail.com>
2025-03-27 23:53:43 +0700

    feat: create footer

---
+++ components/footer.tsx
@@ -0,0 +1,86 @@
+import Link from 'next/link';
+import React from 'react';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import XIcon from '@mui/icons-material/X';
+
+function Footer() {
+  return (
+    <div className="flex flex-col justify-between items-center px-16 py-6 bg-[#111828] text-white">
+      <div className="flex flex-col items-center justify-center gap-12">
+        {/* Above Content */}
+        <div className="grid sm:grid-cols-1 md:grid-cols-4 gap-16">
+          <div className="flex flex-col gap-4 md:gap-2 sm:items-center md:items-start">
+            <h1 className="font-bold text-3xl md:text-xl lg:text-2xl text-center">
+              ShopeMe
+            </h1>
+            <p className="opacity-50 text-center text-xl   md:text-md lg:text-lg font-light md:text-start">
+              Your destination for minimalist design products.
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-4 md:gap-2 sm:items-center md:items-start">
+            <h1 className="font-bold text-3xl md:text-xl lg:text-2xl text-center">
+              Quick Links
+            </h1>
+            <div className="flex flex-col gap-1">
+              <Link
+                href=""
+                className="opacity-50 text-center text-xl   md:text-md lg:text-lg font-light md:text-start"
+              >
+                Home
+              </Link>
+              <Link
+                href=""
+                className="opacity-50 text-center text-xl   md:text-md lg:text-lg font-light md:text-start"
+              >
+                products
+              </Link>
+              <Link
+                href=""
+                className="opacity-50 text-center text-xl   md:text-md lg:text-lg font-light md:text-start"
+              >
+                About
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4 md:gap-2 sm:items-center md:items-start">
+            <h1 className="font-bold text-3xl md:text-xl lg:text-2xl text-center">
+              Contact
+            </h1>
+            <div className="flex flex-col gap-1">
+              <p className="opacity-50 text-center text-xl   md:text-md lg:text-lg font-light md:text-start">
+                Email: ShopMe@gmail.com
+              </p>
+              <p className="opacity-50 text-center text-xl   md:text-md lg:text-lg font-light md:text-start">
+                Phone: (555) 123-456789
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4 md:gap-2 sm:items-center md:items-start">
+            <h1 className="font-bold text-3xl md:text-xl lg:text-2xl text-center">
+              Follow Us
+            </h1>
+            <div className="flex gap-6 md:gap-2 items-center justify-center">
+              <InstagramIcon
+                sx={{ opacity: 0.5, fontSize: { xs: 48, md: 32 } }}
+              />
+              <FacebookIcon
+                sx={{ opacity: 0.5, fontSize: { xs: 48, md: 32 } }}
+              />
+              <XIcon sx={{ opacity: 0.5, fontSize: { xs: 48, md: 32 } }} />
+            </div>
+          </div>
+        </div>
+        {/* Bottom Content */}
+        <div>
+          <p> © 2025 ShopMe. All rights reserved </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
